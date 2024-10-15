0ab71af3ce44a35370e44e978ea9b3f4a0f87dd7
MakaelaRM <makaela.maryanski@gmail.com>
2024-10-15 13:51:34 -0600

    cleaned up code to retrieve file

--- authorization.py
+++ authorization.py
@@ -29,3 +29,21 @@ def build_api():
     
     service = build('drive', 'v3', credentials=temp_creds)
     return service
+
+def find_file(file_id):
+
+    try:
+        # create drive api client
+        service = build_api()
+
+        # Use the files().get() method to retrieve the file metadata
+        csv_file = service.files().get(fileId=file_id).execute()
+
+        # Print the file details
+        print("Yayyy file found!!")
+        print("Name:", csv_file.get('name'))
+        print("MIME Type:", csv_file.get('mimeType'))
+        print("ID:", csv_file.get('id'))
+
+    except Exception as e:
+        print(f'Failed to get file: {e}')
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import get_file
+import authorization
 
 file_id = '1eARZ0iioA7GDm1fb_b5Vc9gLhVxxxibf'
 
 if __name__ == '__main__':
   
-  csv_file = get_file.find_file(file_id)
+  csv_file = authorization.find_file(file_id)
