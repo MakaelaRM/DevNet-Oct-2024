dda439495044b062986cf9bd0cd6daed5bc58914
MakaelaRM <makaela.maryanski@gmail.com>
2024-10-15 22:12:14 -0600

    replaced find_file with download_csv method

--- authorization.py
+++ authorization.py
@@ -1,49 +1,54 @@
-import os.path
+import os
+import io
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
 
 SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
+# gets credentials
 def authenticate():
-    
+
     creds = None
-    
+
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port = 0)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
     return creds
 
+# creates drive api client
 def build_api():
-    
+
+    # credentials
     temp_creds = authenticate()
-    
+
+    # create drive api client
     service = build('drive', 'v3', credentials=temp_creds)
     return service
 
-def find_file(file_id):
+#downloads file
+def download_csv(file_id, destination):
 
+    # creates drive api client
+    service = build_api()
+
+    # gets file from Google drive
+    request = service.files().get_media(fileId=file_id)
+
+    # downloads file from Google dive
     try:
-        # create drive api client
-        service = build_api()
-
-        # Use the files().get() method to retrieve the file metadata
-        csv_file = service.files().get(fileId=file_id).execute()
-
-        # Print the file details
-        print("Yayyy file found!!")
-        print("Name:", csv_file.get('name'))
-        print("MIME Type:", csv_file.get('mimeType'))
-        print("ID:", csv_file.get('id'))
-
+        fh = io.FileIO(destination, 'wb')
     except Exception as e:
-        print(f'Failed to get file: {e}')
+        print(f'Failed to open file : {e}')
+        
+    downloader = MediaIoBaseDownload(fh, request)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import authorization
+import csv_methods
 
 file_id = '1eARZ0iioA7GDm1fb_b5Vc9gLhVxxxibf'
+destination = 'downloaded_file.csv'
 
 if __name__ == '__main__':
   
-  csv_file = authorization.find_file(file_id)
+  csv_file = authorization.download_csv(file_id, destination)
