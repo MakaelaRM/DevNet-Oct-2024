4c782d916a9b0119d56b4d16da3d7e59466c6033
MakaelaRM <makaela.maryanski@gmail.com>
2024-11-10 15:01:03 -0700

    updated requirements.txt

--- csv_methods.py
+++ csv_methods.py
@@ -37,24 +37,3 @@ def get_data(file_name):
         df = pd.read_csv(file_name)
         
         return df
-    
-def get_selected_value(file_name, data_type, date):
- # Load the CSV file into a DataFrame
-    df = pd.read_csv(file_name)
-    
-    # Ensure the 'date_time' column is in datetime format
-    df['date_time'] = pd.to_datetime(df['date_time'])
-    
-    # Filter the DataFrame for rows matching the specified date
-    selected_rows = df[df['date_time'].dt.date == pd.to_datetime(date).date()]
-    
-    # Check if any rows matched the date
-    if not selected_rows.empty:
-        # Retrieve the values in the data_type column for all matching rows
-        specific_values = selected_rows[data_type].tolist()
-        
-        # If you only want the first matching value, use this:
-        # specific_value = specific_values[0]
-        return specific_values  # Return all matching values
-    else:
-        return f"No data found for date: {date}"
