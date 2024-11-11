7dee70325bc253c0a10b5297eece3b77b297417f
MakaelaRM <makaela.maryanski@gmail.com>
2024-11-10 21:17:43 -0700

    Removed tkinter and replaced with PIL Image

--- get_file.py
+++ get_file.py
@@ -1,6 +1,5 @@
 import os
 import io
-from tkinter import Image
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
